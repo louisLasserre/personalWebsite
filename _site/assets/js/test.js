240d865b262282cec09c83c27399308f23f86728
louisLasserre <louis.lasserre@mmibordeaux.com>
2022-02-01 17:42:00 +0100

    finally working

---
+++ _site/assets/js/test.js
@@ -0,0 +1,67 @@
+loader = document.getElementsByClassName("loader");
+loader__element = document.getElementsByClassName("loader__element");
+
+var tl = gsap.timeline({});
+tl.pause();
+tl.set(loader, {
+    display: 'block'
+}).set(loader__element, {
+    transformOrigin: 'center right',
+}).to(loader__element, 0.6, {
+    scaleX: 1,
+    ease: 'expo.inOut',
+    stagger: 0.1,
+}).set(loader__element, {
+    transformOrigin: 'center left',
+}).to(loader__element, 0.6, {
+    scaleX: 0,
+    ease: 'expo.inOut',
+    stagger: -0.1,
+}).set(loader, {
+    display: 'none',
+});
+
+
+
+
+function switchToSecond() {
+    tl.play(0);
+    setTimeout(function(){
+        second.classList.remove("getOff");
+        second.classList.add("show");
+        console.log(main.outerHTML);
+    
+        main.classList.remove("show");
+        main.classList.add("getOff");
+        console.log(second.outerHTML);
+    }, 1000); 
+    
+
+    
+    
+
+}
+
+function switchToFirst() {
+    tl.play(0); 
+    setTimeout(function(){
+        main.classList.remove("getOff");
+        main.classList.add("show");
+        console.log(main.outerHTML);
+        
+        second.classList.remove("show");
+        second.classList.add("getOff");
+        console.log(second.outerHTML);
+    }, 1000);
+     
+}
+
+main = document.getElementById("main");
+second = document.getElementById("second");
+
+
+btnOfMain = document.getElementById("btnMain");
+btnOfSecond = document.getElementById("btnSecond");
+
+btnOfMain.addEventListener('click', switchToSecond);
+btnOfSecond.addEventListener('click', switchToFirst);
