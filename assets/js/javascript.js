44e274678feaab30314ff49de232d44ac3f33c54
louis lasserre <louis.lasserre@mmibordeaux.com>
2022-10-31 19:45:16 +0100

    removed console logs

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,8 +1,8 @@
 
 
-console.log("launch")     
+
 const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
-console.log(vh);
+
 
 Hpx = window.screen.height;
 
@@ -38,16 +38,16 @@ function unpin() {
     ) {
         eleHeader.classList.remove(classes.pinned)
         eleHeader.classList.add(classes.unpinned)
-        
+
         //eleHeader.style.top = Hpx - ((60*100)/Hpx) +"px";
 
         //fleche.style.transform = 'rotate(-180deg)';
-        
+
         fleche.classList.remove(classes.haut)
-        
+
         fleche.classList.add(classes.bas)
     }
-}    
+}
 
 
 
@@ -60,43 +60,35 @@ window.onload = function (e){
 }
 
 H = 0;
-window.onscroll = function (e) {  
-    // called when the window is scrolled.  
-    
+window.onscroll = function (e) {
+    // called when the window is scrolled.
+
     var H = window.scrollY;
-    //console.log(H);
+
     if (H > vh-60) {
         pin();
-        boutonFleche.onclick = function() { 
-            console.log("click bas");
+        boutonFleche.onclick = function() {
+
             window.scrollTo({
                 top: 0,
                 left: 0,
                 behavior: 'smooth'
             });
-            
-        }; 
-        
+
+        };
+
     }else{
-        
+
         unpin();
-        boutonFleche.onclick = function() { 
-            console.log("clickhaut");
+        boutonFleche.onclick = function() {
+
             window.scrollTo({
                 top: vh-59,
                 left: 0,
                 behavior: 'smooth'
             });
-        }; 
+        };
     }
 }
 
 //unpin();
-
-
-
-
-
-
-
-
